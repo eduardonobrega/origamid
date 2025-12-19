46e8b7e970c1dae3c3a5f649213fcc9847b6c675
Eduardo Nóbrega <eduardo.nobrrega@gmail.com>
2025-12-19 19:37:02 -0300

    correção de mensagem no formulário

--- js/form.js
+++ js/form.js
@@ -3,13 +3,12 @@ const botao = document.querySelector("form button")
 
 function formEnviado(res) {
     if (res.ok) {
-        
+        form.innerHTML =
+            "<p class='message'> <span class='sucesso'>Mensagem Enviada</span>, em breve entraremos em contato</p>"
     } else {
         form.innerHTML =
             "<p class='message'><span class='erro'>Erro no envio</span>, você pode enviar diretamente para o nosso email em: <span class='email'>contato@bikcraft.net</span></p>"
     }
-    form.innerHTML =
-            "<p class='message'> <span class='sucesso'>Mensagem Enviada</span>, em breve entraremos em contato</p>"
 }
 
 function enviarForm(event) {
@@ -19,7 +18,7 @@ function enviarForm(event) {
 
     const data = new FormData(form)
 
-    fetch("../enviar.php", {
+    fetch("./enviar.php", {
         method: "POST",
         body: data,
     }).then(formEnviado)
